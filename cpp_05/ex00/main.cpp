f729461fd3931dcb8f4a66437f15df1e69fdc916
Francisco-Zeferino <franciscozeferino06@gmail.com>
2024-06-14 12:56:19 +0100

    even more fixes

--- cpp_05/ex00/main.cpp
+++ cpp_05/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: ffilipe- <ffilipe-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/11 16:46:20 by ffilipe-          #+#    #+#             */
-/*   Updated: 2024/06/12 10:17:30 by ffilipe-         ###   ########.fr       */
+/*   Updated: 2024/06/14 12:54:21 by ffilipe-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,5 +16,9 @@ int main(){
     Bureaucrat a(11);
     Bureaucrat b;
 
+    std::cout << a << std::endl;
+    a.decrementGrade(5);
+    std::cout << a << std::endl;
+    a.incrementGrade(16);
     std::cout << a << std::endl;
 }
