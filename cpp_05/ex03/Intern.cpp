3ff139994e3a76e2641d88d1fc09ca2496843979
Francisco-Zeferino <franciscozeferino06@gmail.com>
2024-06-19 13:53:10 +0100

    allot of fixes to be done

--- cpp_05/ex03/Intern.cpp
+++ cpp_05/ex03/Intern.cpp
@@ -6,13 +6,15 @@
 /*   By: ffilipe- <ffilipe-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/18 11:16:31 by ffilipe-          #+#    #+#             */
-/*   Updated: 2024/06/18 15:46:26 by ffilipe-         ###   ########.fr       */
+/*   Updated: 2024/06/19 10:25:21 by ffilipe-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Intern.hpp"
 
-Intern::Intern(){}
+Intern::Intern(){
+    std::cout << "Intern default constructor called" << std::endl;
+}
 
 Intern::Intern(Intern &copy){
     *this = copy;
@@ -31,7 +33,7 @@ AForm *Intern::makeForm(const std::string formName, const std::string target){
     std::string formNames[3] = {"shrubbery request", "robotomy request", "presidential request"};
     int pos = -1;
     try{
-        for(int i = 0; i < 4; i++){
+        for(int i = 0; i < 3; i++){
             if(formNames[i] == formName)
                 pos = i;
         }
--- cpp_05/ex03/main.cpp
+++ cpp_05/ex03/main.cpp
@@ -6,7 +6,7 @@
 /*   By: ffilipe- <ffilipe-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/11 16:46:20 by ffilipe-          #+#    #+#             */
-/*   Updated: 2024/06/18 15:35:43 by ffilipe-         ###   ########.fr       */
+/*   Updated: 2024/06/19 11:23:48 by ffilipe-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,51 +15,50 @@
 
 int main(){
     Intern someRandomIntern;
-    AForm *PRT; 
-    PRT = someRandomIntern.makeForm("presidential request", "Bender");
-    PRT = someRandomIntern.makeForm("shrubbery request", "Bender");
-    std::cout << PRT->getSignGrade() << std::endl;
-    delete PRT;
-    //std::cout << rrf->getSignGrade() << std::endl;
-    // Bureaucrat test1(9);
-    // Bureaucrat test2("Manel", 2);
-    // Bureaucrat test3("Frn", 150);
-    // Bureaucrat test4("Fren", 140);
-    // ShrubberyCreationForm shrubb("arvores");
-    // RobotomyRequestForm robot("robodrenas");
-    // PresidentialPardonForm president("president");
-
-    // std::cout << "----------Shrubb test----------" << std::endl;
-    // std::cout << shrubb;
-    // std::cout << test1 << std::endl;
-    // std::cout << test2 << std::endl;
-    // std::cout << test3 << std::endl;
-    // std::cout << test4 << std::endl;
-    // shrubb.execute(test1);
-    // test2.executeForm(shrubb);
-    // shrubb.execute(test3);
-    // shrubb.execute(test4);
-    // std::cout << "-------------------------------" << std::endl;
-    // std::cout << "----------Robotomy test----------" << std::endl;
-    // std::cout << robot;
-    // std::cout << test1 << std::endl;
-    // std::cout << test2 << std::endl;
-    // std::cout << test3 << std::endl;
-    // std::cout << test4 << std::endl;
-    // robot.execute(test1);
-    // test2.executeForm(robot);
-    // robot.execute(test3);
-    // robot.execute(test4);
-    // std::cout << "-------------------------------" << std::endl;
-    // std::cout << "----------Presidential Test----------" << std::endl;
-    // std::cout << president;
-    // std::cout << test1 << std::endl;
-    // std::cout << test2 << std::endl;
-    // std::cout << test3 << std::endl;
-    // std::cout << test4 << std::endl;
-    // president.execute(test1);
-    // test2.executeForm(president);
-    // president.execute(test3);
-    // president.execute(test4);
-    // std::cout << "-------------------------------------" << std::endl;
+    Bureaucrat test1(9);
+    Bureaucrat test2("Manel", 2);
+    Bureaucrat test3("Fran", 150);
+    Bureaucrat test4("Zeca", 140);
+    AForm *robot;
+    AForm *shrubb;
+    AForm *president;
+    robot = someRandomIntern.makeForm("robotomy request", "Bender");
+    shrubb = someRandomIntern.makeForm("shrubbery request", "shrubb");
+    president = someRandomIntern.makeForm("presidential request", "mr president");
+    std::cout << "----------Shrubb test----------" << std::endl;
+    std::cout << *shrubb;
+    std::cout << test1 << std::endl;
+    std::cout << test2 << std::endl;
+    std::cout << test3 << std::endl;
+    std::cout << test4 << std::endl;
+    shrubb->execute(test1);
+    test2.executeForm(*shrubb);
+    shrubb->execute(test3);
+    shrubb->execute(test4);
+    std::cout << "-------------------------------" << std::endl;
+    std::cout << "----------Robotomy test----------" << std::endl;
+    std::cout << *robot;
+    std::cout << test1 << std::endl;
+    std::cout << test2 << std::endl;
+    std::cout << test3 << std::endl;
+    std::cout << test4 << std::endl;
+    robot->execute(test1);
+    test2.executeForm(*robot);
+    robot->execute(test3);
+    robot->execute(test4);
+    std::cout << "-------------------------------" << std::endl;
+    std::cout << "----------Presidential Test----------" << std::endl;
+    std::cout << *president;
+    std::cout << test1 << std::endl;
+    std::cout << test2 << std::endl;
+    std::cout << test3 << std::endl;
+    std::cout << test4 << std::endl;
+    president->execute(test1);
+    test2.executeForm(*president);
+    president->execute(test3);
+    president->execute(test4);
+    std::cout << "-------------------------------------" << std::endl;
+    delete shrubb;
+    delete president;
+    delete robot;
 }
