60a063ec1f91f5d0915d3ae3d3693f5aef1b1fa4
Francisco-Zeferino <franciscozeferino06@gmail.com>
2024-08-23 18:45:57 +0100

    changes

--- cpp_06/ex00/ScalarConverter.cpp
+++ cpp_06/ex00/ScalarConverter.cpp
@@ -6,7 +6,7 @@
 /*   By: ffilipe- <ffilipe-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/26 15:27:04 by ffilipe-          #+#    #+#             */
-/*   Updated: 2024/07/11 12:24:32 by ffilipe-         ###   ########.fr       */
+/*   Updated: 2024/08/23 17:05:58 by ffilipe-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,65 +28,91 @@ ScalarConverter &ScalarConverter::operator=(ScalarConverter const &copy){
     return *this;
 }
 
+bool checkFloat(std::string str){
+    char *end;
+    strtof(str.c_str(),&end);
+    if(*end == 'f' || *(end + 1) == '\0' || errno != ERANGE)
+        return true;
+    return false;
+}
+
+bool checkDouble(std::string str){
+    char *end;
+    strtod(str.c_str(), &end);
+    if(*end == '\0' || errno != ERANGE)
+        return true;
+    return false;
+}
+
 ConvertType getConvertType(std::string str){
-    if(str.find_last_of('.') != std::string::npos && str.find_last_of('f') != std::string::npos)
-        return FLOAT;
-    else if(str == "-inff" || str == "+inff" || str == "nanf" || str == "-inf" || str == "+inf" || str == "nan")
-        return PSLITERAL;
-    else if(str.find_last_of('.') != std::string::npos)
-        return DOUBLE;
-    else if(str.length() == 1 && !isdigit(str[0]))
+    if(isprint(str[0]) && !str[1] && !isdigit(str[0]))
         return CHAR;
-    else if(isdigit(str[0]) || str[0] == '-')
-        return INT;
+    else if(checkFloat(str) == true)
+        return DOUBLE;
+    else if(checkDouble(str) == true)
+        return FLOAT;
+    return NONE;
+}
+
+void checkValidChar(char c){
+    if(isprint(c))
+        std::cout << "char: " << c << std::endl;
+    else if(c >= 0 && c <= 127)
+        std::cout << "char: Non displayable" << std::endl;
     else
-        return NONE;
+        std::cout << "char: impossible" << std::endl;
 }
 
 void convertInt(std::string str){
+    std::cout << "In Int" << std::endl;
     int convertedString = std::atoi(str.c_str());
     float toFloat = static_cast<float>(convertedString);
     double toDouble = static_cast<double>(convertedString);
     char toChar = static_cast<char>(convertedString);
-    std::cout << "char: " << toChar << std::endl;
+    checkValidChar(toChar);
     std::cout << "int: " << convertedString << std::endl;
     std::cout << "float: " << toFloat << ".0f" << std::endl;
     std::cout << "double: " << toDouble << ".0" << std::endl;
 }
 
 void convertFloat(std::string str){
-    float convertedString = std::atof(str.c_str());
+    std::cout << "In Float" << std::endl;
+    float convertedString = std::strtof(str.c_str(), NULL);
     char toChar = static_cast<char>(convertedString);
     double toDouble = static_cast<double>(convertedString);
     int toInt = static_cast<int>(convertedString);
-    std::cout << "char: " << toChar << std::endl;
+    checkValidChar(toChar); 
     std::cout << "int: " << toInt << std::endl;
+    std::cout << std::fixed << std::setprecision(1);
     std::cout << "float: " << convertedString << "f" << std::endl;
     std::cout << "double: " << toDouble << std::endl;
 }
 
 void convertDouble(std::string str){
+    std::cout << "In Double" << std::endl;
     double convertedString = std::atof(str.c_str());
     char toChar = static_cast<char>(convertedString);
     float toFloat = static_cast<float>(convertedString);
     int toInt = static_cast<int>(convertedString);
-    std::cout << "char: " << toChar << std::endl;
+    checkValidChar(toChar);
     std::cout << "int: " << toInt << std::endl;
     std::cout << "float: " << toFloat << "f" << std::endl;
     std::cout << "double: " << convertedString << std::endl;
 }
 
 void convertChar(std::string str){
+    std::cout << "In Char" << std::endl;
     int toInt = static_cast<int>(str[0]);
     float toFloat = static_cast<float>(str[0]);
     double toDouble = static_cast<double>(str[0]);
-    std::cout << "char: " << str << std::endl;
+    checkValidChar(str[0]);
     std::cout << "int: " << toInt << std::endl;
     std::cout << "float: " << toFloat << ".0f" << std::endl;
     std::cout << "double: " << toDouble << ".0" << std::endl;
 }
 
 void convertPsLiteral(std::string str){
+    std::cout << "In PS" << std::endl;
     std::cout << "char: impossible" << std::endl;
     std::cout << "int: impossible" << std::endl;
     if(str == "-inff" || str == "-inf"){
--- cpp_06/ex00/ScalarConverter.hpp
+++ cpp_06/ex00/ScalarConverter.hpp
@@ -6,17 +6,21 @@
 /*   By: ffilipe- <ffilipe-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/26 15:23:30 by ffilipe-          #+#    #+#             */
-/*   Updated: 2024/06/26 16:04:16 by ffilipe-         ###   ########.fr       */
+/*   Updated: 2024/08/23 16:07:45 by ffilipe-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef SCALARCONVERTER_HPP
 #define SCALARCONVERTER_HPP
 
-# include <string>
-# include <iostream>
-# include <limits>
-# include <cstdlib>
+#include <iostream>
+#include <string>
+#include <cstdlib>
+#include <cerrno>
+#include <iomanip>
+#include <cmath>
+#include <limits>
+#include <climits>
 
 enum ConvertType{
     INT,
