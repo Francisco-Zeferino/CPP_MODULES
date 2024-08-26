0ccf94eda01d72a6ffff6699cd7e34ba84df27f8
Francisco-Zeferino <franciscozeferino06@gmail.com>
2024-08-26 17:17:48 +0100

    Major Fixes

--- cpp_06/ex00/ScalarConverter.cpp
+++ cpp_06/ex00/ScalarConverter.cpp
@@ -6,7 +6,7 @@
 /*   By: ffilipe- <ffilipe-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/26 15:27:04 by ffilipe-          #+#    #+#             */
-/*   Updated: 2024/08/23 17:05:58 by ffilipe-         ###   ########.fr       */
+/*   Updated: 2024/08/26 17:16:34 by ffilipe-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,52 +31,75 @@ ScalarConverter &ScalarConverter::operator=(ScalarConverter const &copy){
 bool checkFloat(std::string str){
     char *end;
     strtof(str.c_str(),&end);
-    if(*end == 'f' || *(end + 1) == '\0' || errno != ERANGE)
-        return true;
-    return false;
+    if(*end != 'f' || *(end + 1) != '\0' || errno == ERANGE)
+        return false;
+    return true;
 }
 
 bool checkDouble(std::string str){
     char *end;
     strtod(str.c_str(), &end);
-    if(*end == '\0' || errno != ERANGE)
-        return true;
+    if(*end != '\0' || errno == ERANGE)
+        return false;
+    return true;
+}
+
+bool checkInt(std::string str){
+    char *end;
+    long x = strtol(str.c_str(), &end, 10);
+    if(*end != '\0' || x > INT_MAX || x < INT_MIN || errno == ERANGE)
+        return false;
+    return true;
+}
+
+bool checkLiteral(std::string str){
+    std::string validLiteral[] = {"-inff", "-inf", "+inff", "+inf", "nan", "nanf"};
+    for(int i = 0; i < 6; i++){
+        if(str == validLiteral[i])
+            return true;
+    }
     return false;
 }
 
-ConvertType getConvertType(std::string str){
+ConvertType getConvertType(std::string str){ 
     if(isprint(str[0]) && !str[1] && !isdigit(str[0]))
         return CHAR;
-    else if(checkFloat(str) == true)
-        return DOUBLE;
-    else if(checkDouble(str) == true)
+    else if(checkInt(str))
+        return INT;
+    else if(checkFloat(str))
         return FLOAT;
+    else if(checkDouble(str))
+        return DOUBLE;
+    else if(checkLiteral(str))
+        return PSLITERAL;
     return NONE;
 }
 
 void checkValidChar(char c){
-    if(isprint(c))
-        std::cout << "char: " << c << std::endl;
-    else if(c >= 0 && c <= 127)
+    if(c >= 0 && c <= 127)
         std::cout << "char: Non displayable" << std::endl;
+    else if(isprint(c))
+        std::cout << "char: '" << c << "'" << std::endl;
     else
         std::cout << "char: impossible" << std::endl;
 }
 
 void convertInt(std::string str){
-    std::cout << "In Int" << std::endl;
-    int convertedString = std::atoi(str.c_str());
+    int convertedString = std::strtol(str.c_str(), NULL, 10);
     float toFloat = static_cast<float>(convertedString);
     double toDouble = static_cast<double>(convertedString);
     char toChar = static_cast<char>(convertedString);
-    checkValidChar(toChar);
+    if(convertedString >= 0 && convertedString <= 127)
+        checkValidChar(toChar);
+    else
+        checkValidChar(-1);
+    std::cout << std::fixed << std::setprecision(1);
     std::cout << "int: " << convertedString << std::endl;
-    std::cout << "float: " << toFloat << ".0f" << std::endl;
-    std::cout << "double: " << toDouble << ".0" << std::endl;
+    std::cout << "float: " << toFloat << "f" << std::endl;
+    std::cout << "double: " << toDouble << std::endl;
 }
 
 void convertFloat(std::string str){
-    std::cout << "In Float" << std::endl;
     float convertedString = std::strtof(str.c_str(), NULL);
     char toChar = static_cast<char>(convertedString);
     double toDouble = static_cast<double>(convertedString);
@@ -89,30 +112,29 @@ void convertFloat(std::string str){
 }
 
 void convertDouble(std::string str){
-    std::cout << "In Double" << std::endl;
-    double convertedString = std::atof(str.c_str());
+    double convertedString = std::strtod(str.c_str(), NULL);
     char toChar = static_cast<char>(convertedString);
     float toFloat = static_cast<float>(convertedString);
     int toInt = static_cast<int>(convertedString);
     checkValidChar(toChar);
+    std::cout << std::fixed << std::setprecision(1);
     std::cout << "int: " << toInt << std::endl;
     std::cout << "float: " << toFloat << "f" << std::endl;
     std::cout << "double: " << convertedString << std::endl;
 }
 
 void convertChar(std::string str){
-    std::cout << "In Char" << std::endl;
     int toInt = static_cast<int>(str[0]);
     float toFloat = static_cast<float>(str[0]);
     double toDouble = static_cast<double>(str[0]);
     checkValidChar(str[0]);
+    std::cout << std::fixed << std::setprecision(1);
     std::cout << "int: " << toInt << std::endl;
-    std::cout << "float: " << toFloat << ".0f" << std::endl;
-    std::cout << "double: " << toDouble << ".0" << std::endl;
+    std::cout << "float: " << toFloat << "f" << std::endl;
+    std::cout << "double: " << toDouble << std::endl;
 }
 
 void convertPsLiteral(std::string str){
-    std::cout << "In PS" << std::endl;
     std::cout << "char: impossible" << std::endl;
     std::cout << "int: impossible" << std::endl;
     if(str == "-inff" || str == "-inf"){
@@ -120,8 +142,8 @@ void convertPsLiteral(std::string str){
         std::cout << "double: -inf" << std::endl;
     }
     else if(str == "+inff" || str == "+inf"){
-        std::cout << "float: inff" << std::endl;
-        std::cout << "double: inf" << std::endl;
+        std::cout << "float: +inff" << std::endl;
+        std::cout << "double: +inf" << std::endl;
     }
     else{
         std::cout << "float: nan" << std::endl;
